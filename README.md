3b8fbd34c28193bc814f3817a729af18ff5c03bc
Hassan Abudu <hassanabudu@Hassans-MacBook-Pro.local>
2020-07-15 21:16:59 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Blue Binder
+
+A notebook/blog, chronicling my (Hassan Abudu's) 100 days of Gatsby.
+
+Should I pair this with the reading of the book? I dunno, what do you think? What you should do is shot me an email at <meyian@gmail.com> and let me know if I should, or shouldn't. (Do it, it'll be fun for both of us!)
+
+Other than that, here's the blog in the wild:
+
+
+
---
+++ gatsby-config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Blue Binder | 100 days of Gatsby challenge',
+    description: 'A blog detailing my 100 days of learning Gatsby',
+    keywords: 'gatsbyjs, gatsby, blog',
+    siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
+    author: {
+      name: 'Hassan Abudu',
+      url: 'https://twitter.com/hassanabudu',
+      email: 'meyian@gmail.com'
+    }
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content`
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1rem'
+            }
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1140,
+              quality: 90,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
+      }
+    },
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet'
+  ]
+}
